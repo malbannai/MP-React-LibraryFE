6afb131c04c903036e22e2ef6230374269173992
Lailz <lailaabdulraheem@gmail.com>
2021-02-03 09:01:19 +0300

    add data

---
+++ members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    firstName: "Aziz",
+    lastName: "AlSaffar",
+    slug: "aziz-alSaffar",
+    borrowedBooks: [8],
+    membership: "gold", // can borrow 3 books
+  },
+  {
+    id: 3,
+    firstName: "Alhamza",
+    lastName: "Abdulmonem",
+    slug: "alhmaza-bdulmonem",
+    borrowedBooks: [9, 12],
+    membership: "silver", // can borrow 2 books
+  },
+  {
+    id: 5,
+    firstName: "Laila",
+    lastName: "AlKandery",
+    slug: "laila-alkandery",
+    borrowedBooks: [11, 25],
+    membership: "platinum", // can borrow 5 books
+  },
+];
+
+export default members;
